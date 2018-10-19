a3fcaeb04fc94b134bea30300f789c4b810a04a1
mbatllesm <martibatlles1999@gmail.com>
2018-10-19 09:19:25 +0200

    8.0.0 : API 26 6.0.1 : API 23 ?.?.? : API 19

---
+++ app/src/main/java/com/example/martiportatil/negocis6icons/Business.java
@@ -0,0 +1,13 @@
+package com.example.martiportatil.negocis6icons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Business extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business);
+    }
+}
---
+++ app/src/main/java/com/example/martiportatil/negocis6icons/Knowledge.java
@@ -0,0 +1,13 @@
+package com.example.martiportatil.negocis6icons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Knowledge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_knowledge);
+    }
+}
