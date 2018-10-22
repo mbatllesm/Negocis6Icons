1878db6b55faca5e3f0711cf7150c85d937bfe24
mbatllesm <martibatlles1999@gmail.com>
2018-10-22 08:14:18 +0200

    8.0.0 : API 26 6.0.1 : API 23 ?.?.? : API 19

--- app/src/main/java/com/example/martiportatil/negocis6icons/MainActivity.java
+++ app/src/main/java/com/example/martiportatil/negocis6icons/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.martiportatil.negocis6icons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //this.getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
