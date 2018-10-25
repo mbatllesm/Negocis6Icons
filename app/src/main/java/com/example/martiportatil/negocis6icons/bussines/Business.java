d2a8d03e8ddfff58bb9e2b48b49a2dc4022c0b65
mbatllesm <martibatlles1999@gmail.com>
2018-10-25 09:17:57 +0200

    8.0.0 : API 26 6.0.1 : API 23 ?.?.? : API 19

--- app/src/main/java/com/example/martiportatil/negocis6icons/bussines/Business.java
+++ app/src/main/java/com/example/martiportatil/negocis6icons/bussines/Business.java
@@ -1,18 +1,33 @@
 package com.example.martiportatil.negocis6icons.bussines;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.martiportatil.negocis6icons.MainActivity;
 import com.example.martiportatil.negocis6icons.R;
 
-public class Business extends AppCompatActivity {
-
+public class Business extends AppCompatActivity implements View.OnClickListener {
+    private ImageView b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        b1 = findViewById(R.id.back);
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==b1)
+        {
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
