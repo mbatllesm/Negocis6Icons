f6052ff6a235bcfa61adf6996e4c4ccfdd9663da
mbatllesm <martibatlles1999@gmail.com>
2018-10-26 08:36:01 +0200

    8.0.0 : API 26 6.0.1 : API 23 ?.?.? : API 19

--- app/src/main/java/com/example/martiportatil/negocis6icons/bussines/Business.java
+++ app/src/main/java/com/example/martiportatil/negocis6icons/bussines/Business.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 import com.example.martiportatil.negocis6icons.MainActivity;
 import com.example.martiportatil.negocis6icons.R;
 
 public class Business extends AppCompatActivity implements View.OnClickListener {
     private ImageView b1;
+    private TextView titole;
+    private EditText buscar;
+    private String textsearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.getSupportActionBar().hide();
@@ -20,8 +25,23 @@ public class Business extends AppCompatActivity implements View.OnClickListener 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         b1 = findViewById(R.id.back);
         b1.setOnClickListener(this);
+        titole = findViewById(R.id.titol);
+        buscar = findViewById(R.id.search);
+        buscar.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    Buscador();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
-
+    private void Buscador() {
+        textsearch = buscar.getText().toString();
+        titole.setText(textsearch);
+    }
     @Override
     public void onClick(View v) {
         if (v==b1)
