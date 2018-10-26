7363b450a414e35df64f7aadf48cba8b9847c73f
mbatllesm <martibatlles1999@gmail.com>
2018-10-26 09:44:07 +0200

    8.0.0 : API 26 6.0.1 : API 23 ?.?.? : API 19

--- app/src/main/java/com/example/martiportatil/negocis6icons/bussines/Business.java
+++ app/src/main/java/com/example/martiportatil/negocis6icons/bussines/Business.java
@@ -4,16 +4,24 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.example.martiportatil.negocis6icons.MainActivity;
 import com.example.martiportatil.negocis6icons.R;
 
 public class Business extends AppCompatActivity implements View.OnClickListener {
     private ImageView b1;
+    private LinearLayout Lgralla;
+    private LinearLayout Lautoescola_granollers;
+    private LinearLayout Lgerneral_optica;
+    private TextView Tgralla;
+    private TextView Tautoescola_granollers;
+    private TextView Tgerneral_optica;
     private TextView titole;
     private EditText buscar;
     private String textsearch;
@@ -24,6 +32,12 @@ public class Business extends AppCompatActivity implements View.OnClickListener 
         setContentView(R.layout.activity_business);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         b1 = findViewById(R.id.back);
+        Lgralla = findViewById(R.id.layout_la_gralla);
+        Lautoescola_granollers = findViewById(R.id.layout_autoescola_granollers);
+        Lgerneral_optica = findViewById(R.id.layout_general_optica);
+        Tgralla = findViewById(R.id.text_la_gralla);
+        Tautoescola_granollers = findViewById(R.id.text_autoescola_granollers);
+        Tgerneral_optica = findViewById(R.id.text_general_optica);
         b1.setOnClickListener(this);
         titole = findViewById(R.id.titol);
         buscar = findViewById(R.id.search);
@@ -40,7 +54,27 @@ public class Business extends AppCompatActivity implements View.OnClickListener 
     }
     private void Buscador() {
         textsearch = buscar.getText().toString();
-        titole.setText(textsearch);
+        if (textsearch.equalsIgnoreCase(Tgerneral_optica.getText().toString()))
+        {
+            Lgerneral_optica.setVisibility(View.VISIBLE);
+            Lautoescola_granollers.setVisibility(View.INVISIBLE);
+            Lgralla.setVisibility(View.INVISIBLE);
+        }else if (textsearch.equalsIgnoreCase(Tautoescola_granollers.getText().toString()))
+        {
+            Lautoescola_granollers.setVisibility(View.VISIBLE);
+            Lgralla.setVisibility(View.INVISIBLE);
+            Lgerneral_optica.setVisibility(View.INVISIBLE);
+        }else if (textsearch.equalsIgnoreCase(Tgralla.getText().toString()))
+        {
+            Lgralla.setVisibility(View.VISIBLE);
+            Lgerneral_optica.setVisibility(View.INVISIBLE);
+            Lautoescola_granollers.setVisibility(View.INVISIBLE);
+        }else
+        {
+            Lgralla.setVisibility(View.VISIBLE);
+            Lgerneral_optica.setVisibility(View.VISIBLE);
+            Lautoescola_granollers.setVisibility(View.VISIBLE);
+        }
     }
     @Override
     public void onClick(View v) {
