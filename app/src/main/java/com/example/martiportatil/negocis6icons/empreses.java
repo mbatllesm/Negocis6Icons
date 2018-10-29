5fb884840490958b879b8832cf9c676f65d8e3b5
mbatllesm <martibatlles1999@gmail.com>
2018-10-29 14:12:11 +0100

    XD

--- app/src/main/java/com/example/martiportatil/negocis6icons/empreses.java
+++ app/src/main/java/com/example/martiportatil/negocis6icons/empreses.java
@@ -1,11 +1,16 @@
 package com.example.martiportatil.negocis6icons;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
-public class empreses extends AppCompatActivity {
+import com.example.martiportatil.negocis6icons.bussines.Business;
+
+public class empreses extends AppCompatActivity implements View.OnClickListener{
+    private ImageView b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         String empresa1 = getIntent().getExtras().getString("empresa");
@@ -28,5 +33,11 @@ public class empreses extends AppCompatActivity {
             image.setImageResource(R.drawable.autoescola_granollers);
             //setContentView(image);
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+            Intent intent = new Intent(this,Business.class);
+            startActivity(intent);
     }
 }
