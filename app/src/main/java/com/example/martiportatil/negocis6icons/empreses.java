c42b936593df584105e7fe1b49b9f9e3b16c27d3
mbatllesm <martibatlles1999@gmail.com>
2018-10-29 13:25:23 +0100

    XD

--- app/src/main/java/com/example/martiportatil/negocis6icons/empreses.java
+++ app/src/main/java/com/example/martiportatil/negocis6icons/empreses.java
@@ -1,13 +1,16 @@
 package com.example.martiportatil.negocis6icons;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class empreses extends AppCompatActivity {
-
+    private String empresa1 = getIntent().getStringExtra("empresa");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empreses);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
