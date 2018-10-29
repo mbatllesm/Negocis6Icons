457ca4898ee0a8eb845d23dc8d6cb1e59c869a01
mbatllesm <martibatlles1999@gmail.com>
2018-10-29 14:01:15 +0100

    XD

--- app/src/main/java/com/example/martiportatil/negocis6icons/empreses.java
+++ app/src/main/java/com/example/martiportatil/negocis6icons/empreses.java
@@ -6,27 +6,27 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class empreses extends AppCompatActivity {
-    private String empresa1 = getIntent().getStringExtra("empresa");
-    private ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String empresa1 = getIntent().getExtras().getString("empresa");
         this.getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
+        ImageView image;
         setContentView(R.layout.activity_empreses);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         image = findViewById(R.id.img);
-        if (empresa1 == "La Gralla")
+        if (empresa1.equalsIgnoreCase("La Gralla"))
         {
             image.setImageResource(R.drawable.la_gralla);
-            setContentView(image);
-        }else if(empresa1 == "General Optica")
+            //setContentView(image);
+        }else if(empresa1.equalsIgnoreCase("General Optica"))
         {
             image.setImageResource(R.drawable.general_optica);
-            setContentView(image);
-        }else if(empresa1 == "Autoescola Granollers")
+            //setContentView(image);
+        }else if(empresa1.equalsIgnoreCase("Autoescola Granollers"))
         {
             image.setImageResource(R.drawable.autoescola_granollers);
-            setContentView(image);
+            //setContentView(image);
         }
     }
 }
